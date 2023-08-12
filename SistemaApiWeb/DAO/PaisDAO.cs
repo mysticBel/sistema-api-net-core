07e10223ce20a8d67b818c17a374cb7dea098118
mysticBel <dnxbel@gmail.com>
2023-08-12 12:29:33 -0500

    DAO Pais GetPais()

--- SistemaApiWeb/DAO/PaisDAO.cs
+++ SistemaApiWeb/DAO/PaisDAO.cs
@@ -1,13 +1,41 @@
-﻿namespace SistemaApiWeb.DAO
+﻿using SistemaApiWeb.Entidades;
+using Microsoft.Data.SqlClient;
+
+namespace SistemaApiWeb.DAO
 {
     public class PaisDAO
     {
         private string cadena;
+        //Constructor
         public PaisDAO()
         {
 
             cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("cn");
         }
+
+        //GetPaises
+        public IEnumerable<Pais> GetPaises()
+        {
+            List<Pais> paisList = new List<Pais>();
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("exec sp_listar_paises", cn);
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    paisList.Add(new Pais()
+                    {
+                        idpais = dr.GetInt32(0),
+                        nombrepais = dr.GetString(1)
+
+                    });
+                }
+               
+            }
+            return paisList;
+        }
+
 
     }
 }
