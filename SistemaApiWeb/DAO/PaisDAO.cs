86a78e084b98e705e358c7273f536a258fef9fbd
mysticBel <dnxbel@gmail.com>
2023-08-12 11:24:06 -0500

    Agregar archivos de proyecto.

---
+++ SistemaApiWeb/DAO/PaisDAO.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaApiWeb.DAO
+{
+    public class PaisDAO
+    {
+        private string cadena;
+        public PaisDAO()
+        {
+
+            cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("cn");
+        }
+
+    }
+}
---
+++ SistemaApiWeb/Entidades/Pais.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaApiWeb.Entidades
+{
+    public class Pais
+    {
+        public int idpais { get; set; }
+        public string nombrepais { get; set; }
+    }
+}
