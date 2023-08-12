e1a7e2590bccc01727cff2bd74a78310c7466a39
mysticBel <dnxbel@gmail.com>
2023-08-12 12:29:09 -0500

    modelo cliente

---
+++ SistemaApiWeb/Entidades/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaApiWeb.Entidades
+{
+    public class Cliente
+    {
+        public int idcliente { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public int idpais { get; set; }
+        public int telefono { get; set; }
+
+    }
+}
