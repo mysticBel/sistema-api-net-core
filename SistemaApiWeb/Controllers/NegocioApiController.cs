e47fd1456d9070fa09b07e144d89a5b4272e21df
mysticBel <dnxbel@gmail.com>
2023-08-12 13:07:51 -0500

    Controlador: [HttpPost("AddCliente")], [HttpPut("UpdateCliente")]

--- SistemaApiWeb/Controllers/NegocioApiController.cs
+++ SistemaApiWeb/Controllers/NegocioApiController.cs
@@ -11,11 +11,44 @@ namespace SistemaApiWeb.Controllers
     [ApiController]
     public class NegocioApiController : ControllerBase
     {
+        //Listar Paises
         [HttpGet("GetPaises")]
         public async Task<ActionResult<List<Pais>>> GetPais() 
         {
             var lista = await Task.Run(() => (new PaisDAO()).GetPaises());
             return Ok(lista);  //okObjectResult rpta
         }
+
+
+        // CLiente CRUD
+        [HttpGet("GetClientes")]
+        public async Task<ActionResult<List<Cliente>>> GetCliente()
+        {
+            var lista = await Task.Run(() => (new ClienteDAO()).GetClientes());
+            return Ok(lista);
+        }
+
+        [HttpPost("AddCliente")]
+        public async Task<ActionResult<String>> InsertarCliente(Cliente cliente)
+        {
+            var mensaje = await Task.Run(() => (new ClienteDAO()).Agregar(cliente));
+            return Ok(mensaje);
+        }
+
+        [HttpPut("UpdateCliente")]
+        public async Task<ActionResult<String>> ActualizarCliente(Cliente cliente)
+        {
+            var mensaje = await Task.Run(() => (new ClienteDAO()).Actualizar(cliente));
+            return Ok(mensaje);
+        }
+
+
     }
 }
+
+
+
+
+
+
+
