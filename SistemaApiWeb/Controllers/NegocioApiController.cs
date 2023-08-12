4e11fb633400dd7962acede2e07eb2ae1803e0f1
mysticBel <dnxbel@gmail.com>
2023-08-12 12:30:02 -0500

    Controlador GetPais

---
+++ SistemaApiWeb/Controllers/NegocioApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaApiWeb.Entidades;
+
+using SistemaApiWeb.Entidades;
+using SistemaApiWeb.DAO;
+
+namespace SistemaApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NegocioApiController : ControllerBase
+    {
+        [HttpGet("GetPaises")]
+        public async Task<ActionResult<List<Pais>>> GetPais() 
+        {
+            var lista = await Task.Run(() => (new PaisDAO()).GetPaises());
+            return Ok(lista);  //okObjectResult rpta
+        }
+    }
+}
